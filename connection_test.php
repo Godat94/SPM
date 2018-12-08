971d07a8342aa1c7644710b1ae1bf18fcef2fef6
Godat94 <36738160+Godat94@users.noreply.github.com>
2018-12-08 16:45:38 +0100

    new code

--- connection_test.php
+++ connection_test.php
@@ -1,29 +1,45 @@
+<?php
+
+// From the blog-post
 $connectstr_dbhost = '127.0.0.1:52941';
 $connectstr_dbname = 'localdb';
 $connectstr_dbusername = 'azure';
 $connectstr_dbpassword = 'password';
 
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+foreach ($_SERVER as $key => $value)
+{
+    if (strpos($key, "MYSQLCONNSTR_") !== 0)
+    {
         continue;
     }
-    
+
     $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
     $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
     $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+
+    echo $value;
 }
 
-$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+define('DB_NAME', $connectstr_dbname);
+define('DB_USER', $connectstr_dbusername);
+define('DB_PASSWORD', $connectstr_dbpassword);
+define('DB_HOST', $connectstr_dbhost);
 
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+// Custom testing
+echo $connectstr_dbname."\n";
+echo $connectstr_dbusername."\n";
+echo $connectstr_dbpassword."\n";
+echo $connectstr_dbhost."\n";
+
+$dbConnection = $dbLink = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if (!$dbConnection)
+{
+    echo "Failed to connect to the database.\n";
 }
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
+else
+{
+    echo "Successfully connected to the database.\n";
+}
+?>
