cddb95f3ada95abaf2e7331dd5e04709b5ae6728
Godat94 <36738160+Godat94@users.noreply.github.com>
2019-02-07 14:12:36 +0100

    Update fileManager.php

--- fileManager.php
+++ fileManager.php
@@ -18,5 +18,45 @@
 	});
 	</script>
 </head>
+<body>
+<!-- Navigation -->
+<?php require_once 'elements/nav_menu.php'; ?> 
 
+<header class="bg-primary text-white">
+	<div class="container text-center">
+		<h1>MANAGE BPMN FILES</h1>
+	</div>
+</header>
+<section id="about">
+	<div class="container">
+		<!-- Display status message -->
+		<?php if(!empty($statusMsg) && ($statusMsgType == 'success')){ ?>
+		<div class="col-xs-12">
+			<div class="alert alert-success"><?php echo $statusMsg; ?></div>
+		</div>
+		<?php }elseif(!empty($statusMsg) && ($statusMsgType == 'error')){ ?>
+		<div class="col-xs-12">
+			<div class="alert alert-danger"><?php echo $statusMsg; ?></div>
+		</div>
+		<?php } ?>
+		
+		<div class="row">
+			<div class="col-md-12 search-panel">
+				<!-- Search form -->
+				<form>
+					<div class="input-group mb-3">
+						<input type="text" name="searchKeyword" class="form-control" placeholder="Search by title..." value="<?php echo $searchKeyword; ?>">
+						<div class="input-group-append">
+						  <input type="submit" name="submitSearch" class="btn btn-outline-secondary" value="Search">
+						  <a href="<?php echo BASE_URL.'fileManager.php'; ?>" class="btn btn-outline-secondary">Reset</a>
+						</div>
+					</div>
+				</form>
+				
+				
+
+<!-- Footer -->
+<?php require_once 'elements/footer.php'; ?> 
+
+</body>
 </html>
